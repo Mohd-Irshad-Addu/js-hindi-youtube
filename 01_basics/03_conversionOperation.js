fee2f0c50ba889c718137514a6601143683f0d64
Mohd Irshad <94297731+Mohd-Irshad-Addu@users.noreply.github.com>
2023-09-05 13:41:13 +0000

    converstion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+console.log("case 1 ")
+let score = 33
+
+console.log(typeof score);
+// or
+console.log(typeof (score));            //both are same
+
+// converting string into number
+let myscore = "22"
+//lets convert "22" into a number.
+let myscoreInNumber = Number(myscore)
+console.log(myscoreInNumber);
+console.log("")     //for space
+
+console.log("case 2 ")
+let score2 = "hitech"
+console.log(typeof(score2))
+
+let score2InNumber = Number(score2)
+console.log(typeof(score2InNumber))
+console.log(score2InNumber);
+
+console.log("")     //for space
+
+console.log("case 3 ")
+let score3 = null
+console.log(typeof(score3))
+
+let score3InNumber = Number(score3)
+console.log(typeof(score3InNumber))
+console.log(score3InNumber)
+
+console.log("")     //for space
+
+
+console.log("case 4 ")
+let score4 = "32efc"
+console.log(typeof(score4))
+
+let score4InNumber = Number(score4)
+console.log(typeof(score4InNumber))
+console.log(score4InNumber)
+
+console.log("")     //for space
+
+/*
+true will convert as 1
+and
+false will convert as 0
+*/
+
+console.log("case 5 "); //conversion of number into string
+let mynumber = 44;
+console.log(typeof(mynumber))
+
+let mynumberString = String(mynumber)
+console.log(mynumberString);
+console.log(typeof(mynumberString));
+
