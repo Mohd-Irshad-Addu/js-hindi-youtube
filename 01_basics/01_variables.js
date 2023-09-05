4e1f71e5c519ea6315acee0358def4489153188d
Mohd Irshad <94297731+Mohd-Irshad-Addu@users.noreply.github.com>
2023-09-05 12:51:32 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const empId = 12438
+let empName = "irshad";
+var empage = 23
+city = "Hyderabad"
+
+//best way to print all variables 
+console.table([empId,empName,empage,city])
+
+
+//understand const , let, var 
+//const == once declares cannt be edited
+//let == can declare only once but easily we can modify
+        //empName = "salman"
+        //console.table([empId,empName,empage,city])
+/*
+prefer not to use var
+because we can face block issue 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+//Number == 2 to power of 53 or 52
+//bigInt
+//string == ""
+//boolean == true/false
+//null == standalone value
+//undefined == have to define
+//symbol = unique
+
+
+
+//object
